c59222a014e3f6f3144dade6961679c063de44da
Mat191126 <matprz11@gmail.com>
2020-03-30 10:32:31 +0200

    added TodoItem function as in description

--- TodoItem.java
+++ TodoItem.java
@@ -57,4 +57,32 @@ public class TodoItem{
     private String title;
     private LocalDate deadline;
     private boolean isDone;
+
+    public TodoItem(String title, LocalDate deadline){
+        this.title = title;
+        this.deadline = deadline;
+        isDone = false;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+    public LocalDate getDeadline(){
+        return deadline;
+    }
+
+    public void mark(){
+        isDone = true;
+    }
+    public void unmark(){
+        isDone = false;
+    }
+
+    public String toString(){
+        String markCharacter = isDone ? "x" : " ";
+        int day = deadline.getDayOfMonth();
+        int month = deadline.getMonthValue();
+        String todoItemString = String.format("[%s] %s-%s %s", markCharacter, day, month, title);
+        return todoItemString;
+    }
 }
